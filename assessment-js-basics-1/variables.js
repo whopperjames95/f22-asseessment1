ab2164648ccdd4c44ab0129f40f45b0d9dc1601a
James Goff <james.goff1030@gmail.com>
2022-04-15 10:06:41 -0700

    Problems 1-5 completed

--- assessment-js-basics-1/variables.js
+++ assessment-js-basics-1/variables.js
@@ -3,15 +3,21 @@
 
 //CODE HERE 
 
+let myName = "James Goff";
+
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
 
+let faveNum = 1030;
+
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +27,12 @@
 */
 
 //CODE HERE
+if(faveNum=== 13){
+  console.log('lucky 13!')
+} else {
+  console.log('not lucky 13!')
+}
+
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +40,6 @@
 */
 
 //CODE HERE
+for(let i = 0; i< 5; i++){
+  console.log(faveNum)
+}
