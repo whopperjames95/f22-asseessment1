9cf09742b224dcfd4598962b3339dd43341b9080
James Goff <james.goff1030@gmail.com>
2022-04-15 12:00:13 -0700

    all problems completed

--- assessment-js-basics-1/arrays.js
+++ assessment-js-basics-1/arrays.js
@@ -41,3 +41,12 @@ console.log(middleNums)
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+const answers = []
+
+for (let i =0; i < bigOrSmallArray.length; i++){
+  if(bigOrSmallArray[i] > 100){
+    answers.push('big')
+  } else{
+    answers.push('small')
+  }
+} console.log(answers)
